ba699dbe476f6ab82c38d16c09cad41cf3c76f09
bye1158 <9522159@gmail.com>
2025-12-31 10:56:04 +0800

    Update workers.js

--- workers.js
+++ workers.js
@@ -6,7 +6,6 @@ const HTML_CONTENT = `
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Card Tab</title>
     <link rel="icon" type="image/svg+xml" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><polygon points='50,5 61,38 95,38 67,58 78,92 50,72 22,92 33,58 5,38 39,38' fill='black'/></svg>">
-
     <style>
     /* 全局样式 */
     body {
