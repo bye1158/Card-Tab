7780ec05d81720de0b555e0c5b6c9bfa4aded73b
bye1158 <9522159@gmail.com>
2025-12-31 10:37:18 +0800

    Delete bookmarks-export-addons directory

--- bookmarks-export-addons/bookmark-exporter/popup/popup.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const bookmarkService = new BookmarkService();
-    const exportService = new ExportService();
-
-    document.getElementById('exportBtn').addEventListener('click', async () => {
-        const bookmarks = await bookmarkService.getBookmarks();
-        exportService.exportToJson(bookmarks);
-    });
-}); 
